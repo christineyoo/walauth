4b69b2d96ee1a3386790e611a0ea1cf82966d1c0
Christine Yoo <christinecho.s@gmail.com>
2021-10-07 15:15:49 -0700

    update gitignore

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+//MongoDB connection here
+module.exports = {
+    MongoURI: 'mongodb+srv://christine123:yoochristine70@test-cluster1.q91t5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
