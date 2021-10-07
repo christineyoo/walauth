beed1ff3555bb47ef286b99a774132d935c2fe01
Christine Yoo <christinecho.s@gmail.com>
2021-10-07 14:07:56 -0700

    call next() in middleware

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: "babel-eslint",
+      env: {
+          es6: true
+      },
+      globals: {
+      supertest: true,
+      expect: true
+    }
+  };
+  
--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('passport')
 const app = express()
+
+//Passport config
+require('./config/passport')(passport)
 
 //Database config and connect to Mongo
 const db = require('./config/keys').MongoURI
@@ -14,17 +18,24 @@ mongoose.connect(db, { useNewUrlParser: true })
 //Bodyparser
 app.use(express.urlencoded({ extended: false }))
 
-//Express session and connect with flash
+//Express session
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
 }))
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash())
+
 //Global variables to use custom color flash messages
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
+    next()
 })
 
 //EJS
